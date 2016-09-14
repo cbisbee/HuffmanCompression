b97b0b1cc136347c71d31a8c32e28732a7bf98e8
Charles Bisbee <bisbeecharlesl@gmail.com>
2016-09-14 17:39:35 -0600

    Implemented node struct for Huffman tree nodes, added function to generate the initial priority queue from the frequency list.

--- HuffmanCompression/HuffmanCompression/HuffmanCompressionSource.cpp
+++ HuffmanCompression/HuffmanCompression/HuffmanCompressionSource.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <functional>
+#include <queue>
 
 using namespace std;
+
+struct huffNode
+{
+	int frequency;
+	char character;
+	huffNode* left;
+	huffNode* right;
+
+	huffNode()
+	{
+		this->frequency = -1;
+		this->character = '?';
+		this->left = NULL;
+		this->right = NULL;
+	}
+
+	huffNode(int freq, char ch,  huffNode* L, huffNode* R)
+	{
+		this->frequency = freq;
+		this->character = ch;
+		this->left = L;
+		this->right = R;
+	}
+
+	bool friend operator>(const huffNode& A,const huffNode& B)
+	{
+		return (A.frequency > B.frequency);
+	}
+};
+
+void generateInitialPQueue(int frequencyList[256], priority_queue<huffNode, vector<huffNode>, greater<huffNode>>& nodeHeap)
+{
+	for (int i = 0; i < 256; i++)
+	{
+		if (frequencyList[i] != 0)
+		{
+			huffNode tempNode(frequencyList[i], (char)i, NULL, NULL);// = new huffNode;
+			nodeHeap.push(tempNode);
+		}
+	}
+}
+
+void printPQueue(priority_queue<huffNode, vector<huffNode>, greater<huffNode>> nodeHeap)
+{
+	if (nodeHeap.empty())
+	{
+		cout << "There's nothing in the heap!" << endl;
+	}
+	while (!nodeHeap.empty())
+	{
+		huffNode tempNode;
+		tempNode = nodeHeap.top();
+		cout << tempNode.character << endl;
+		nodeHeap.pop();
+	}
+}
 
 void initializeFrequencyList(int frequencyList[256])
 {
@@ -56,9 +114,13 @@ int main(int argc, char* argv[])
 			string outFileName = inFileName.substr(0, inFileName.length() - 3) + "mcp"; //assumes a .txt file (changes output to a .mcp) 
 			ofstream fout(outFileName, ios::binary | ios::out);
 			int frequencyList[256];
+			priority_queue<huffNode, vector<huffNode>, greater<huffNode> > nodeHeap;
+
 			initializeFrequencyList(frequencyList);
 			generateFrequencyList(fin, frequencyList);
-			printFrequencyList(frequencyList);
+			//printFrequencyList(frequencyList);
+			generateInitialPQueue(frequencyList, nodeHeap);
+			//printPQueue(nodeHeap);
 
 
 			//read the file, get all characters and counts of 
