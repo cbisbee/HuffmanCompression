e4eb3907e348e93d5b15ddb0ce0785314b349e50
Charles Bisbee <bisbeecharlesl@gmail.com>
2016-09-14 16:37:12 -0600

    Completed function to create a frequency list for all characters in text file. Added test file to test said function.

--- HuffmanCompression/HuffmanCompression/HuffmanCompressionSource.cpp
+++ HuffmanCompression/HuffmanCompression/HuffmanCompressionSource.cpp
@@ -4,6 +4,39 @@
 
 using namespace std;
 
+void initializeFrequencyList(int frequencyList[256])
+{
+	for (int i = 0; i < 256; i++)
+		frequencyList[i] = 0;
+}
+
+void generateFrequencyList(ifstream& fin, int frequencyList[256])
+{
+	string line = "";
+	while (!fin.eof())
+	{
+		getline(fin,line);
+		for (int i = 0; i < line.length(); i++)
+		{
+			frequencyList[line[i]] += 1;
+		}
+		frequencyList[10] += 1;
+		line = "";
+	}
+}
+
+void printFrequencyList(int frequencyList[256])
+{
+	for (int i = 0; i < 256; i++)
+	{
+		if (frequencyList[i] != 0)
+		{
+			cout << "Character " << (char)i << ": " << frequencyList[i] << endl;
+			//cout << "ASCII " << i << ": " << frequencyList[i] << endl;
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 2)
@@ -20,11 +53,27 @@ int main(int argc, char* argv[])
 			cout << "Could not open target file, please rerun the program with a valid text file (.txt)." << endl;
 		else
 		{
-			string outFileName = inFileName.substr(0, inFileName.length() - 3) + "mcp"; //assumes a .txt file 
-			
+			string outFileName = inFileName.substr(0, inFileName.length() - 3) + "mcp"; //assumes a .txt file (changes output to a .mcp) 
+			ofstream fout(outFileName, ios::binary | ios::out);
+			int frequencyList[256];
+			initializeFrequencyList(frequencyList);
+			generateFrequencyList(fin, frequencyList);
+			printFrequencyList(frequencyList);
+
+
+			//read the file, get all characters and counts of 
+
+			//make create frequency list
+
+			//create huffman tree
+
 			//run the compression algorithm
+
 		}
 	}
+
+	cin.get();
+	cin.get();
 
 	return 0;
 }
